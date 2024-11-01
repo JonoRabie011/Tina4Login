5f06e5511b59084d93461b297d4b8fb8b2d6a9f5
Jonathan Rabie <jonathan.rabie@toss-group.com>
2024-11-01 09:43:58 +0100

    Loafing module file

--- loadModule.php
+++ loadModule.php
@@ -1,5 +1,5 @@
 <?php
 
-\Tina4\Module::addModule("Tina4 Login Module", "0.0.1", "tina4components/tina4login", function(\Tina4\Config $config) {
+\Tina4\Module::addModule("Tina4 Login Module", "0.0.1", "Tina4Login", function(\Tina4\Config $config) {
     $config->addTwigGlobal('baseTwigFile', $_ENV["BASE_TWIG_FILE"] ?? 'tina4Login-base.twig');
 });
